80cdda08be3d73299daa188e4dd44afd0256f459
Jon <jonflanagan.jf@icould.com>
2020-09-17 14:54:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.set('view engine','ejs');
 
 
 //create and provide the server port for the app
-app.listen(8080, function(){
+app.listen(1337, function(){
 	console.log("Server is running correctly");
 });
 
